9d661bb40efff33c53e6521a4b7cb997d4dfe67b
Stefan <stefann.kostovski@gmail.com>
2026-01-05 22:27:25 +0100

    Pri kliknuti sa zmeni dataset z false na true

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const symbols = ['🐶', '🐱', '🐭', '🐹', '🐰', '🦊', '🐶', '🐱', '🐭', '🐹', '🐰', '🦊']
-const main = document.querySelector(".main")
+let main = document.querySelector(".main")
 
 
-symbols.map(sybol => {
+const hodnoty = symbols.map(sybol => {
     const newDiv = document.createElement("div")
+    newDiv.dataset.isActive = "false"
+    newDiv.textContent = "?"
     newDiv.className = "card"
-    newDiv.textContent = sybol
     main.appendChild(newDiv)
+    return newDiv.textContent
 })
+
+
+main.addEventListener("click",function (e){
+    /*Menim data set z false na true */
+    console.log(e.target.dataset)
+    e.target.dataset.isActive ="true"
+    console.log(e.target.dataset)
+})
+
